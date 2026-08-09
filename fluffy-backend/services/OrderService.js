88fe6bbd70a5b79138cc5d4d85fd908a1e8282f2
salsabil ahmed <slsbylahmed86@gmail.com>
2026-08-09 22:21:32 +0300

    Fix: Correct start script path and unify module imports

--- fluffy-backend/services/OrderService.js
+++ fluffy-backend/services/OrderService.js
@@ -1,5 +1,5 @@
-const Order = require('../models/Order');
-const Product = require('../models/Product');
+import Order from '../models/Order.js';
+import Product from '../models/Product.js';
 
 class OrderService {
   
